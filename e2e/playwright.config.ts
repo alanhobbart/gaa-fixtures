d6b97761963a61fecf431a7d9142859ed36c8086
alanhobbart <65234747+alanhobbart@users.noreply.github.com>
2025-03-04 17:55:44 +0000

    test CI (#1)
    * .
    
    * fix test
    
    * update test to expect 'Gaa fixtures' in h1 title
    
    * e2e fix
    
    * add e2e UI test command and comment out unused browser configurations

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
+/*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -44,7 +44,7 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
+*/
     // Uncomment for mobile browsers support
     /* {
       name: 'Mobile Chrome',
